72e91c6405df3ce4feaed18339caeea59c6eb063
emma-dougherty <doughertyemma@hotmail.com>
2022-05-03 16:34:58 +0100

    Lovely jubbly

--- models/decorator.js
+++ models/decorator.js
@@ -3,7 +3,23 @@ const Decorator = function () {
 }
 
 Decorator.prototype.addPaintCan = function (paint_can){
+    this.stock.push(paint_can)
+}
 
+Decorator.prototype.totalLitres = function(){
+    let total = 0
+    for (paint of this.stock) {
+        total += paint.volume
+    }
+    return total
+}
+
+Decorator.prototype.enoughPaint = function (room){
+    if (room.area > this.totalLitres()){
+        return false
+    } else {
+        return true
+    }
 }
 
 module.exports = Decorator 
--- models/specs/decorator_specs.js
+++ models/specs/decorator_specs.js
@@ -1,14 +1,17 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js')
 const PaintCan = require('../paint_can.js')
+const Room = require('../room.js')
 
 describe ('Decorator' , function(){
     let decorator;
     let paintcan;
+    let room;
 
     this.beforeEach(function (){
         decorator = new Decorator()
         paintcan = new PaintCan(5)
+        room = new Room (25)
     })
 
     it('Should start with an empty paint stock', function(){
@@ -19,8 +22,21 @@ describe ('Decorator' , function(){
     it('Should be able a can of paint to the paint stock' ,function (){
         decorator.addPaintCan(paintcan)
         const actual = decorator.stock
-
+        assert.deepEqual (actual,[paintcan])
     } )
 
+    it ('Should be able to calculate total litres of paint in stock', function (){
+        decorator.addPaintCan(paintcan)
+        decorator.addPaintCan(paintcan)
+        const actual = decorator.totalLitres()
+        assert.strictEqual (actual, 10)
+    })
+
+    it('Should be able to if there is enough paint to paint a room', function (){
+        decorator.addPaintCan(paintcan)
+        decorator.addPaintCan(paintcan)
+        const actual = decorator.enoughPaint(room)
+        assert.strictEqual (actual, false)
+    })
 }
 )
